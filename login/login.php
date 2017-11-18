873a3fb5d28122e2d4fe3459fb4aca449bc2b710
samuka942 <samu.gyorgy@mndsblog.com>
2017-11-18 19:54:42 +0100

    Login ablak bevezetése

--- login/login.php
+++ login/login.php
@@ -1,4 +1,7 @@
 <?php
+	session_start();
+?>
+<?php
 	
 	$zarotag = "</body></html>";
 	
@@ -14,6 +17,7 @@
 	
 	$connect = @mysql_connect($server,$user,$pass) or exit("A csatlakozás nem sikerült");
 	@mysql_select_db("mndsblog") or exit("Nem sikerült az adatbázishoz csatlakozni.");
+	mysql_query("SET NAMES utf8");
 	
 	$username = $_POST['username'];
 	$password = $_POST['password'];
@@ -26,28 +30,23 @@
     	$users[] = $sor;
 	}
 	
-	$login_correct = false;
+	$login_correct = FALSE;
 	
 	foreach($users as $user){
-		if($username == $user['username'] && $password == $user['passwords']){
-			$login_correct = true;
-			$email = $user['email'];
+		if($username == $user['login_name'] && $password == $user['user_password']){
+			$login_correct = TRUE;
+			$_SESSION['name'] = $user['user_name'];
+			$_SESSION['email'] = $user['email_address'];
+			$_SESSION['nametag'] = $user['user_name_code'];
 		}
 	}
 	
 	if($login_correct == TRUE){	
-		if(!isset($_COOKIE['login'])){
-			setcookie("login","$username",time()+3600);
-		}
-		else{
-			unset($_COOKIE);
-			setcookie("login","$username",time()+3600);
-		}
-		header("Location: ../menu/index.html");
+		header("Location: ../menu/index.php");
 	}
 	else{
 		die("A bejelentkezés sikertelen!<br><a href='index.html'>Vissza a bejelentkezéshez</a>");
 	}
-	
+	var_dump($_SESSION);
 	
 ?>
